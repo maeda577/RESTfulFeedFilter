cfed5e603701d1e9d5a108388ae8f5eef2fa7182
maeda577 <kazu55632000@gmail.com>
2022-07-24 13:46:59 +0900

    メモ

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
@@ -13,6 +16,11 @@ internal class Program
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddHttpLogging(logging =>
+        {
+            logging.LoggingFields = HttpLoggingFields.All;
+        });
+
         // 標準の出力を切ってXMLに限定する https://docs.microsoft.com/ja-jp/aspnet/core/web-api/advanced/formatting?view=aspnetcore-6.0
         builder.Services.AddControllers(options =>
         {
@@ -20,13 +28,28 @@ internal class Program
             options.OutputFormatters.RemoveType<SystemTextJsonOutputFormatter>();
         }).AddXmlSerializerFormatters();
 
+        // XML出力でXML Declarationが消えないようにする
+        builder.Services.Configure<MvcOptions>(options =>
+        {
+            XmlSerializerOutputFormatter? xmlWriterSettings = options.OutputFormatters
+                .OfType<XmlSerializerOutputFormatter>()
+                .FirstOrDefault();
+            if (xmlWriterSettings != null)
+            {
+                xmlWriterSettings.WriterSettings.OmitXmlDeclaration = false;
+                xmlWriterSettings.WriterSettings.Indent = true;
+                // xmlWriterSettings.WriterSettings.NewLineChars = "¥r¥n";
+                // xmlWriterSettings.WriterSettings.NewLineHandling = System.Xml.NewLineHandling.Entitize;
+            }
+        });
+
         // Swaggerの設定 https://docs.microsoft.com/ja-jp/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-6.0
         builder.Services.AddSwaggerGen(options =>
         {
             options.SwaggerDoc("v1", new OpenApiInfo
             {
-                Version = "1.0.0",
-                
+                Version = "1.0.1",
+
                 Title = "RESTful Feed Filter",
                 Description = "This is a simple API to exclude unnecessary elements from RSS, Atom, and other feeds. Unnecessary elements are specified by XPath.",
                 Contact = new OpenApiContact
@@ -45,6 +68,10 @@ internal class Program
         });
 
         WebApplication app = builder.Build();
+        app.UseHttpLogging();
+
+        // 静的ファイルの提供 https://docs.microsoft.com/ja-jp/aspnet/core/fundamentals/static-files?view=aspnetcore-6.0
+        app.UseFileServer();
 
         // SwaggerのjsonとWebUIを有効化
         app.UseSwagger();
